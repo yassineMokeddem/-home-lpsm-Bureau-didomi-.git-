8f55f6e4bb78ec8c942500502a9e931f22ae0621
yassineMokeddem <51960582+yassineMokeddem@users.noreply.github.com>
2020-04-21 12:34:40 +0200

    Add files via upload

--- spark-app.py
+++ spark-app.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext, SQLContext
-from pyspark.sql.functions import col, countDistinct, SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
 
 
 def flatten_df(nested_df):
@@ -40,20 +41,22 @@ if __name__ == '__main__':
 	sc = spark.sparkContext
 	sqlcontext = SQLContext(sc)
 
+	file_json1 = '/home/lpsm/Bureau/didomi/datehour=2020-01-21-00/part-00000-3a6f210d-7806-462e-b32c-344dff538d70.c000.json'
+	file_json2 = '/home/lpsm/Bureau/didomi/datehour=2020-01-21-01/part-00000-9b503de5-0d72-4099-a388-7fdcf6e16edb.c000.json'
 
-	datafile_json1 = '/home/lpsm/Bureau/didomi/datehour=2020-01-21-00/part-00000-3a6f210d-7806-462e-b32c-344dff538d70.c000.json'
-	datafile_json2 = '/home/lpsm/Bureau/didomi/datehour=2020-01-21-01/part-00000-9b503de5-0d72-4099-a388-7fdcf6e16edb.c000.json'
+	json_df1 = flatten_df(sqlcontext.read.json(file_json1))
+	json_df2 = flatten_df(sqlcontext.read.json(file_json2))
+	json_df = json_df1.union(json_df2)
 
-	data1 = flatten_df(sqlcontext.read.json(datafile_json1))
-	data2 = flatten_df(sqlcontext.read.json(datafile_json2))
-	data = data1.union(data2)
+	df_pageviews = json_df.filter(json_df['type'] == 'pageview')
+	pageviews = df_pageviews.count()
+	pageviews_with_consent = df_pageviews.filter(json_df['user_consent'] == True).count()
 
-	pageviews = data.filter(data['type'] == 'pageview').count()
-	pageviews_with_consent = data.filter(data['type'] == 'pageview').filter(data['user_consent'] == True).count()
-	consents_asked = data.filter(data['type'] == 'consent.asked').count()
-	consents_asked_with_consent = data.filter(data['type'] == 'consent.asked').filter(data['user_consent'] == True).count()
-	consents_given = data.filter(data['type'] == 'consent.given').count()
-	consents_given_with_consent =  data.filter(data['type'] == 'consent.given').filter(data['user_consent'] == True).count()
-	users = data.agg(countDistinct(col("user_id")).alias("count")).collect()[0][0]
+	df_consent_asked = json_df.filter(json_df['type'] == 'consent.asked')
+	consents_asked = df_consent_asked.count()
+	consents_asked_with_consent = df_consent_asked.filter(json_df['user_consent'] == True).count()
 
-
+	df_consent_given = json_df.filter(json_df['type'] == 'consent.given')
+	consents_given = df_consent_given.count()
+	consents_given_with_consent =  df_consent_given.filter(json_df['user_consent'] == True).count()
+	users = json_df.select(("user_id")).distinct().count()
